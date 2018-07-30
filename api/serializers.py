aab0a4273f98ce73454c6eb45dd1dda7317b4cd8
Dario Mavec <dario.mavec@gmail.com>
2018-07-30 14:58:59 +1000

    Updated serializer to return more friendly values for frontend.

--- api/serializers.py
+++ api/serializers.py
@@ -10,22 +10,34 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class IngredientSerializer(serializers.ModelSerializer):
-    name = serializers.CharField()
-    unit = serializers.CharField()
-    type = serializers.CharField()
+    name = serializers.SerializerMethodField()
+    unit = serializers.SerializerMethodField()
+    type = serializers.SerializerMethodField()
 
     class Meta:
         model = Ingredient
         fields = ('name', 'unit', 'type')
 
+    def get_name(self, obj):
+        return obj.name.title()
+
+    def get_unit(self, obj):
+        return obj.unit if obj.unit != 'none' else ''
+
+    def get_type(self, obj):
+        return obj.type.title()
+
 
 class ListSerializer(serializers.HyperlinkedModelSerializer):
     ingredient = IngredientSerializer()
-    quantity = serializers.DecimalField(max_digits=10, decimal_places=2)
+    quantity = serializers.SerializerMethodField()
 
     class Meta:
         model = List
         fields = ('ingredient', 'quantity',)
+
+    def get_quantity(self, obj):
+        return round(100 * obj.quantity) / 100
 
 
 class RecipeSerializer(serializers.HyperlinkedModelSerializer):
